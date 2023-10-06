bac45a9a877ca0cd92e29fe8a89e70753fd3a97b
savyan12 <savyan12mohamed@gmail.com>
2023-10-06 18:00:37 +0300

    Edit the file

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:savyan12/sufyan.git main
+# git push -f git@github.com:savyan12/sufyan.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:savyan12/sufyan.git main:gh-pages
 
 cd -
