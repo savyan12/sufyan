dde934f5d4a28540675bc809f7000a5f83188dad
savyan12 <savyan12mohamed@gmail.com>
2023-10-07 12:31:58 +0300

    add to netlify

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
-// vue.config.js file to be placed in the root of your repository
-
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/sufyan/'
-    : '/'
+  pwa: {
+      workboxOptions: {
+        exclude: [/_redirects/]
+      }
+    }
 }
